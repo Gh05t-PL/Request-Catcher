c9c2ce9d6ac860d1b16f9395a93cdd4cd17bef8a
gh05t-pl <gozetero@gmail.com>
2020-04-06 18:02:31 +0200

    excluded access to dashboard from ngrok

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+function isNgrok(headers) {
+  return headers.host.includes('ngrok');
+}
+
 /* GET home page. */
 router.all('/', function(req, res, next) {
+  if ( isNgrok(req.headers) ) {
+    res.send('');
+    return;
+  }
+
   res.render('dashboard', { title: 'Express' });
 });
 
